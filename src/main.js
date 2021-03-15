91e80c863d61c571822abd78b09a8506a97d74ba
linyu995 <1021469672@qq.com>
2021-03-15 15:51:54 +0800

    插件案例

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import ANT from './util/direactiveOne'
 import Pluign from './util/pluginvue'
-
 Vue.use(ANT)
 Vue.use(Pluign)
 Vue.config.productionTip = false
